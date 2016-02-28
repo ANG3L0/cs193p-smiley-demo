8c74827babba080ba445f7c1918bcfd5981190e5
Ugghhh <angelowo@umich.edu>
2016-02-27 18:32:55 -0800

    Lectures 5 and 6 with notes for gestues, delegation, drawing

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,113 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Angelo Wong on 2/27/16.
+//  Copyright © 2016 Stanford. All rights reserved.
+//
+
+import UIKit
+
+protocol FaceViewDataSource: class {
+    func smilinessForFaceView(sender: FaceView) -> Double?
+}
+
+@IBDesignable
+
+class FaceView: UIView {
+    
+    @IBInspectable
+    var lineWidth: CGFloat = 3 {
+        didSet {
+            setNeedsDisplay() //redraw when width changes
+        }
+    }
+    @IBInspectable
+    var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+    @IBInspectable
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
+
+    //weak says don't let this var be keeping whatever it is referencing in memory.
+    //if this is not set, dataSource will force controller to be in memory, and
+    //controller, if it has an outlet to FaceView will always keep FaceView in memory, which
+    //would force controller to always be in memory (cannot ever free memory)
+    weak var dataSource: FaceViewDataSource?
+    
+    func scale (gesture: UIPinchGestureRecognizer) {
+        if gesture.state == .Changed {
+            scale *= gesture.scale
+            gesture.scale = 1
+        }
+    }
+    
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    
+    private struct Scaling {
+        static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
+        static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
+        static let FaceRadiusToEyeSeparationRatio: CGFloat = 1.5
+        static let FaceRadiusToMouthWidthRatio: CGFloat = 1
+        static let FaceRadiusToMouthHeightRatio: CGFloat = 3
+        static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
+    }
+    
+    private enum Eye { case Left, Right }
+    
+    private func bezierPathForEye(whichEye: Eye) -> UIBezierPath {
+        let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+        
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeVerticalOffset
+        switch whichEye {
+        case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+        case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+        }
+        
+        
+        let path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private func bezierPathForSmile(fractionOfMaxSmile: Double) -> UIBezierPath {
+        let mouthWidth = faceRadius / Scaling.FaceRadiusToMouthWidthRatio
+        let mouthHeight = faceRadius / Scaling.FaceRadiusToMouthHeightRatio
+        let mouthVerticalOffset = faceRadius / Scaling.FaceRadiusToMouthOffsetRatio
+        
+        let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
+        
+        let start = CGPoint(x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthVerticalOffset)
+        let end = CGPoint(x: start.x + mouthWidth, y: start.y)
+        let cp1 = CGPoint(x: start.x + mouthWidth / 3, y: start.y + smileHeight)
+        let cp2 = CGPoint(x: end.x - mouthWidth / 3, y: cp1.y)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(start)
+        path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = lineWidth
+        return path
+    }
+
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+        
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+        
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0
+        let smilePath = bezierPathForSmile(smiliness)
+        smilePath.stroke()
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# cs193p-pa1
+
+## Intent
+Mainly to summarize lectures 5 and 6's demo of the smiley face app.  This app allows you to make the person happier by panning up, and vice versa.  Also allows for zooming in and out.
+
+## Summary
+Around 3 hours of lectures, so I will try my best to butcher this the least I can:
+* Protocols: FaceViewDataSource creates a binding contract between View and Controller where View asks Controller about how much to smile.
+* Drawing stuff: Using CGPoint and CGFloat (not regular float/ints/doubles!) to get a frame of reference for the FaceView.  Using UIBezier to draw circles and arcs for face, eyes, and mouth.
+* Gestures: Two ways of doing gestures -- one is explicitly adding an "addGestureRecognizer" under the FaceView IBOutlet.  The other way is to just ctrl-drag an IBAction from the storyboard.
+  * Zoom in/out has Controller listen to View to see if there's any pinching going on.  If there is, it will go ahead and call the View's scaling function which will then zoom the smiley in and out.
+  * The panning gesture is observed by Controller once again, but the happiness calculation is done in the Controller (since that is a Controller property and not a View property).  When the Controller's happiness changes, it asks the View to update the UI.  During the UI update, the 'smiliness' (View's analogy of happiness) will derive its value by delegating the calculation to the Controller once again.  The Controller does so and finally updates the View again.
+    * TL;DR the flow is: Control listens to View -> View starts to update UI -> Controller does calculations -> View can finish updating UI
